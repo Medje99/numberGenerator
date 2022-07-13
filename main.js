1ec64efb4bb5e4caa60b1c5936f3d34808957d2e
Medje99 <109211529+Medje99@users.noreply.github.com>
2022-07-13 11:26:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+let box = document.getElementById('box');
+let box2 = document.getElementById('box2')
+let input = document.getElementById('check')
+
+
+let x = 0;
+
+function numbers(){ 
+    x = Math.floor(Math.random() * 900) + 100;
+    box2.innerHTML = x;
+
+       
+    setTimeout(() => {
+         box2 = document.getElementById('box2');
+          box2.style.visibility = 'hidden';
+      }, 3000);
+     
+      
+
+      setTimeout(() => {
+         box2 = document.getElementById('box2');
+          box2.style.visibility = 'visible';
+      },);
+
+}
+
+function checkk(){
+    let y = input.value;
+    if(x == ""){
+        alert("Click on 'Get number!")
+    }else if(y == ""){
+        alert('insert number')
+    }else if(y == x){
+        alert('good')
+    }else{
+        alert('bad')
+    }
+}
