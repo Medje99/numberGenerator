9e7a011a2f2f21a6b6270d3627acb20bf13d65c5
Medje99 <109211529+Medje99@users.noreply.github.com>
2022-08-02 12:02:26 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ let input = $('#check');
 let selected = $('#digits');
 let btnNumbers = $('#btnNumbers');
 let submit= $('#submit');
+let x;
 
+
+  selected.on('click', function gg(){
+    let box2 = $('#box2');
+    box2.css('visibility','visible')
+
+    if(selected.val() == "3"){    
+        box2.html('***')
+   }else if(selected.val() == "4"){
+       box2.html('****')
+   }else if(selected.val() == "5"){
+       box2.html('*****')
+   }else if(selected.val() == "6"){
+       box2.html('******')
+   }
+  });
 
 function displayOff(){
     let box2 = $('#box2');
@@ -13,8 +29,11 @@ function displayOff(){
 function displayOn(){
     let box2 = $('#box2');
     box2.css('visibility','visible')
-
 }
+
+
+
+
 
 btnNumbers.on('click', function () {
     
@@ -53,6 +72,7 @@ btnNumbers.on('click', function () {
         box2.html(x)
 
     }
+    return x;
 })
 
 
@@ -65,18 +85,53 @@ let sc = $('#numbers')
 
 submit.on('click', function () {
     
+    let submit= $('#submit');
+    let timer = $('#selectTime');
     let y = input.val();
+
     submit.attr('disabled','disabled');
     
-    if(x == ""){
-        alert("Click on 'Get number!")
+    if(isNaN(x)){
+        alert("Click on 'Get a number!")
     }else if(y == ""){
         alert('insert number')
     }else if(y == x){
-        alert('Correct number, click "Get number" to continue')
-        score++
-        sc.text(`${score}`);
+
+        if(selected.val() == 3){
+            alert('Correct number, click "Get number" to continue')
+            score += 0.5;
+            sc.text(`${score}`);
+        }else if(selected.val() == 4){
+            alert('Correct number, click "Get number" to continue')
+            score++;
+            sc.text(`${score}`);
+        }
+        else if(selected.val() == 5){
+            alert('Correct number, click "Get number" to continue')
+            score += 1.5;
+            sc.text(`${score}`);
+        }
+        else if(selected.val() == 6){
+            alert('Correct number, click "Get number" to continue')
+            score += 2;
+            sc.text(`${score}`);
+        };
+
+        if(timer.val() == "a"){
+            score += 2;
+            sc.text(`${score}`);
+        }else if(timer.val() == "b"){
+            score += 1.5;
+            sc.text(`${score}`);
+        }else if(timer.val() == "c"){
+            score += 1;
+            sc.text(`${score}`);
+        }else if(timer.val() == "d"){
+            score += 0.5;
+            sc.text(`${score}`);
+        }
     }else{
+        
         alert('Wrong number, click "Get number" to continue')
         score = 0;
         sc.text(`${score}`);
@@ -93,7 +148,16 @@ submit.on('click', function () {
  
     
 function tutorial(){
-    alert('The goal of this exercise is to type the same number that you got after clicking "Get number", and collect as many points as possible')
+    alert('The goal of this exercise is to type the same number that you get after clicking "Get number", and collect as many points as possible')
     alert('You can choose the difficulty of the task by changing the number of digits and digits display time.')
+    alert( '0.5 Points for 1s\n1 Points for 0.75s')
 
+}
+
+function infoNum(){
+    alert( '0.5 Points for 1s\n1 Points for 0.75s\n1.5 Points for 0.5s\n2 Points for 0.25s')
+}
+
+function infoNum(){
+    alert( '0.5 Points for 3 digits\n1 Points for 4 digits\n1.5 Points for 5 digits\n2 Points for 6 digits')
 }
